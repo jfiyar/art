72923c591b2779fa7c62f7448625fb8d9027e1aa
jianglikui <jianglikui@himoca.com>
2018-12-15 17:58:11 +0800

    login html

--- src/main/java/com/jfiy/art/controller/Test.java
+++ src/main/java/com/jfiy/art/controller/Test.java
@@ -1,32 +1,32 @@
 package com.jfiy.art.controller;
 
-import com.jfiy.art.model.User;
-import com.jfiy.art.service.UserService;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
-@RestController
+@Controller
 
 public class Test {
 
-    private final UserService userService;
-
-    @Autowired
-    public Test(UserService userService) {
-        this.userService = userService;
-    }
-
-
-    @RequestMapping(value = "/")
-    public String hello() {
-        List<User> list=userService.getUserInfo();
-        for (User u:list) {
-            System.out.println(u.getName());
+    @GetMapping(value = "/")
+    public String Index(HttpServletRequest req) {
+        HttpSession session = req.getSession();
+        if (session.getAttribute("user") == null) {
+            return "index";
         }
-        return "hello,Spring Boot";
+        return "homepage";
+    }
+    @GetMapping(value = "/login.do")
+    public String Login(){
+        return "123";
+    }
+    @GetMapping(value = "/reg")
+    public String Reg(){
+        return "123";
     }
 }
