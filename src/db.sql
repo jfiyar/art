69ef36702c7e45fc02836fd6376b09e5d261d28f
jfiy <jfuy@outlook.com>
2019-03-16 13:01:51 +0800

    fix dbdata

--- src/db.sql
+++ src/db.sql
@@ -1,45 +1,45 @@
-
-
 create table artist
 (
-  user_id    int                                 not null,
+  user_id    int                                 not null
+    primary key,
   nation     varchar(32)                         null,
-  birthday   timestamp default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP,
+  birthday   timestamp default CURRENT_TIMESTAMP not null
+  on update CURRENT_TIMESTAMP,
   profession varchar(32)                         null,
   info       text                                null,
-  review     int       default 0                 not null,
+  review     int default '0'                     not null,
   avatar     varchar(255)                        null,
+  state      varchar(255)                        null,
   constraint artist_user_id_uindex
-    unique (user_id)
+  unique (user_id)
 )
-  charset = utf8;
-
-alter table artist
-  add primary key (user_id);
+  engine = InnoDB;
 
 create table artwork
 (
   id     int auto_increment
     primary key,
-  name   varchar(32)   not null,
-  artist int           null,
-  info   longtext      null,
-  review int default 0 not null,
-  score  int default 0 not null
+  name   varchar(32)                        not null,
+  artist int                                null,
+  info   longtext                           null,
+  review int default '0'                    not null,
+  score  int default '0'                    not null,
+  type   int default '0'                    null,
+  time   datetime default CURRENT_TIMESTAMP null
 )
-  charset = utf8;
+  engine = InnoDB;
 
 create table artwork_comment
 (
   id        int auto_increment
     primary key,
-  reply     int                                 null,
-  user_id   int                                 not null,
-  content   text                                null,
+  reply     int                                null,
+  user_id   int                                not null,
+  content   text                               null,
   timestamp datetime default CURRENT_TIMESTAMP not null,
-  art_id    int                                 null
+  art_id    int                                null
 )
-  charset = utf8;
+  engine = InnoDB;
 
 create table artwork_media
 (
@@ -48,7 +48,18 @@ create table artwork_media
   artwork_id int                         null,
   type       varchar(32) default 'image' null,
   url        varchar(255)                null
-);
+)
+  engine = InnoDB;
+
+create table type
+(
+  id   int auto_increment
+    primary key,
+  name varchar(32) not null,
+  constraint type_name_uindex
+  unique (name)
+)
+  engine = InnoDB;
 
 create table user
 (
@@ -58,12 +69,14 @@ create table user
   pwd      varchar(32)                not null,
   auth     varchar(10) default 'user' not null,
   avatar   varchar(255)               null,
-  sex      int         default 0      null,
-  isArtist tinyint(1)  default 0      not null,
+  sex      int default '0'            null,
+  isArtist tinyint(1) default '0'     not null,
   constraint user_name_uindex
-    unique (name)
+  unique (name)
 )
-  charset = utf8;
+  engine = InnoDB;
+
+
 
 insert into user (name, pwd, auth, isArtist) values ('admin','4297f44b13955235245b2497399d7a93','admin',1);
 
